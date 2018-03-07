3dea925b382319b870bc6a42b6f4416645f87436
TITANCORP\thang.luu <thang.luu@titancorpvn.com>
2018-03-07 16:54:10 +0700

    init code

---
+++ codenvyio.php
@@ -0,0 +1,14 @@
+<?php
+// sudo apt-get remove mysql-server
+// sudo apt-get update
+// sudo apt-get install mysql-server
+// sudo service mysql start
+// sudo /usr/sbin/update-rc.d mysql defaults
+// sudo apt-get install phpmyadmin
+// sudo dpkg-reconfigure mysql-server
+// FLUSH PRIVILEGES; SET PASSWORD FOR root@localhost = '123456';
+// sudo dpkg-reconfigure phpmyadmin  //sudo dpkg-reconfigure mysql-server
+//https://askubuntu.com/questions/118772/how-to-change-root-password-for-mysql-and-phpmyadmin
+//run apt-get update
+phpinfo()
+
